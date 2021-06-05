8d77f3df2b7db11852daa4c78b6b382cc8315c36
Yevgen Pikus <yevgen.pikus@gmail.com>
2021-06-05 12:16:37 +0200

    Set integration profile

--- src/test/java/com/github/cookingbackend/CookingBackendApplicationTests.java
+++ src/test/java/com/github/cookingbackend/CookingBackendApplicationTests.java
@@ -2,8 +2,10 @@ package com.github.cookingbackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("integration")
 class CookingBackendApplicationTests {
 
 	@Test
