120d738475fec9b97bf7465906066bdafa476bbf
Yevgen Pikus <yevgen.pikus@gmail.com>
2021-06-05 12:15:53 +0200

    Add security config

---
+++ src/main/java/com/github/cookingbackend/security/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.github.cookingbackend.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeRequests(authorize ->
+                        {
+                            authorize.mvcMatchers("/api-docs").permitAll();
+                            authorize.mvcMatchers("/**").permitAll();
+                        }
+                );
+    }
+
+}
