81938fe84c717c15c8aaf04caab4b7f109a949b2
Yevgen Pikus <yevgen.pikus@gmail.com>
2021-06-13 20:42:00 +0200

    Make repo interfaces public

--- src/main/java/com/github/cookingbackend/repositories/StepRepository.java
+++ src/main/java/com/github/cookingbackend/repositories/StepRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-interface StepRepository extends JpaRepository<Step, Long> {
+public interface StepRepository extends JpaRepository<Step, Long> {
 }
--- src/main/java/com/github/cookingbackend/repositories/TagRepository.java
+++ src/main/java/com/github/cookingbackend/repositories/TagRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-interface TagRepository extends JpaRepository<Tag, Long> {
+public interface TagRepository extends JpaRepository<Tag, Long> {
 }
